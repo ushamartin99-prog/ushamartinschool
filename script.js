9eee32bbda8534ddf66ce5bccaf3b34aeb0cc812
ushamartin99-prog <ushamartin99@gmail.com>
2026-09-14 16:13:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 // HAMBURGER MENU
+
 const menuBtn = document.getElementById("menuBtn");
 const menu = document.getElementById("menu");
 
 if (menuBtn && menu) {
-    menuBtn.addEventListener("click", function() {
+
+    menuBtn.addEventListener("click", function () {
+
         menu.classList.toggle("show");
+
     });
+
 }
 
 
 // CONTACT FORM
+
 const form = document.querySelector(".contact-page form");
 
 if (form) {
+
     form.addEventListener("submit", function(event) {
+
         event.preventDefault();
 
         alert("Message Sent Successfully!");
 
         form.reset();
+
     });
+
 }
 
 
 // LEARN MORE BUTTON
+
 const learnMoreBtn = document.getElementById("learnMoreBtn");
 
 if (learnMoreBtn) {
+
     learnMoreBtn.addEventListener("click", function() {
+
         window.location.href = "about.html";
+
     });
+
 }
